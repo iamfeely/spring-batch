5e05dfdb8e69efe6f6bb4b4349138e64c000c3a1
P212423 <P212423@SKMNS-P212423.skmns.co.kr>
2021-07-22 16:45:40 +0900

    등록

---
+++ src/main/java/com/redbol/batch/job/StepNextJobConfig.java
@@ -0,0 +1,65 @@
+package com.redbol.batch.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepNextJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    StepNextJobConfig(JobBuilderFactory jobBuilderFactory,StepBuilderFactory stepBuilderFactory){ 
+		this.jobBuilderFactory=jobBuilderFactory;
+		this.stepBuilderFactory=stepBuilderFactory; 
+	}
+    @Bean
+    @Qualifier("stepNextJob")
+    public Job stepNextJob() {
+        return jobBuilderFactory.get("stepNextJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                	System.out.println(">>>>> This is Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                	System.out.println(">>>>> This is Step3");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
